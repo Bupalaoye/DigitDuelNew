95d46ee405f54876a834067fc135e706abcae88d
Dola <3181475781@qq.com>
2025-08-24 17:47:52 +0800

    add new card

--- Scripts/Data/CardData.gd
+++ Scripts/Data/CardData.gd
@@ -6,10 +6,11 @@ class_name CardData
 
 @export var card_id: StringName = &""  # 卡牌的唯一ID, 例如: "ace_of_spades", "joker_red"
 @export var card_name: String = ""     # 用于显示的卡牌名称, 例如: "Ace", "Joker"
-@export var description: String = ""   # 卡牌描述
+
 
 @export_group("prop")
 # 遵循GDScript风格，属性名建议使用小写蛇形命名法 (snake_case)
+@export var cost: int = 0
 @export var atk: int = 0
 @export var def: int = 0
 
@@ -17,6 +18,7 @@ class_name CardData
 # 我们也可以把图片路径存在这里！
 @export var texture_path: String = "" 
 
-@export_group("group")
-@export var suit: String = ""          # 花色: "hearts", "spades", 或对王牌来说为空
-@export var rank: String = ""          # 点数: "ace", "10", 或对王牌来说是 "joker_red"
+@export_group("desc")
+@export var description: String = ""   # 卡牌描述
+@export var rules_text : String = ""
+@export var flavor_text : String = ""
